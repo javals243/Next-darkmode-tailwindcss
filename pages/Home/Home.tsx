b002ec08838132b03fd284c9e6e6a142c8412b2a
javals243 <vchihisa@gmail.com>
2022-08-11 13:03:08 +0200

    minor change

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { showToastMessage } from "../../components/ToastMessage/ToastMessage";
-
+import Image from "next/image";
+import Image1 from "../../public/img/MongoDB.svg";
 export const Home = () => {
   const notify = () => showToastMessage("votre toast custom ", "success");
   return (
-    <div className="flex justify-center items-center flex-col w-full h-[100vh] ">
-      <h1 className=" flex justify-center items-center text-3xl font-bold py-5">
-        Starter NextJs TailwindCss
-      </h1>
-      <button
-        className=" flex justify-center items-center bg-blue-500 px-10 py-3 rounded"
-        onClick={notify}
-      >
-        Toast
-      </button>
-    </div>
+    <>
+      <div className="flex justify-center items-center flex-col w-full h-[100vh] ">
+        <h1 className=" flex justify-center items-center text-3xl font-bold py-5">
+          Starter NextJs TailwindCss
+        </h1>
+        <button
+          className=" flex justify-center items-center bg-blue-500 px-10 py-3 rounded"
+          onClick={notify}
+        >
+          Toast
+        </button>
+      </div>
+    </>
   );
 };
 
--- pages/LoadingPage/LoadingPage.tsx
+++ pages/LoadingPage/LoadingPage.tsx
@@ -1,6 +1,6 @@
 const LoadingPage = () => {
   return (
-    <section className="bg-blue-900 relative place-items-center grid h-screen w-screen gap-4">
+    <section className="bg-white relative place-items-center grid h-screen w-screen gap-4">
       <div className="bg-blue-500 w-48 h-48  absolute animate-ping rounded-full delay-5s shadow-xl"></div>
 
       <div className="bg-blue-400 w-32 h-32 absolute animate-ping rounded-full shadow-xl"></div>
